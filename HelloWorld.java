b1fc15def403ef70b05822e4aecddc278941160d
kksahu81 <kksahu81@gmail.com>
2018-10-07 16:40:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String args[]){
             System.out.println("I'm dancing");
+	    System.out.println("I'm singing");
 	        }
 		}
 		// Execute Thru Jenkins
